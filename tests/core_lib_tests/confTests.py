27aee5dc4aaff30bb0f2b2bfefc90742ac41ac0b
spicytelescope <jbeziaud@gmail.com>
2022-06-07 01:58:50 +0200

    test: developing core tests

--- tests/core_lib_tests/confTests.py
+++ tests/core_lib_tests/confTests.py
@@ -5,17 +5,22 @@ import numpy as np
 
 # ===== Unit testing =====
 
-# Checking good behaviour of the core function after n turns
-INIT_GRID: np.ndarray = np.array([[0, 0, 1]])
-EXPECTED_GRID: np.ndarray = np.array([])  # create a mock result that shoudln't
-N_TURN: int = 5
+# checking good behaviour of the core function after n turns
+NORMAL_INIT_GRID: np.ndarray = np.array([[0, 0, 1]])
+NORMAL_EXPECTED_GRID: np.ndarray = np.array([])
+NORMAL_N_TURN: int = 5
 
-# Checking expected crash for incorrect values in the core grid
+# checking expected crash for incorrect values in the core grid
 INCORRECT_INIT_GRID: np.ndarray = np.array([[3, 2, 1]])
 
-# Checking core grid behaviour with a huge number of turn
+# checking core grid behaviour with a huge number of turn
 LOAD_TEST_N_TURN: int = 100_000_000
 LOAD_TEST_INIT_GRID: np.ndarray = np.array([[0, 0, 1]])
-LOAD_TEST_EXPECTED_GRID: np.ndarray = np.array(
-    []
-)  # create a mock result that shoudln't
+LOAD_TEST_EXPECTED_GRID: np.ndarray = np.array([])
+
+# create a mock result that is expected not to yield out living cells
+NO_LIVING_INIT_GRID: np.ndarray = np.array([[0, 0, 1]])
+NO_LIVING_EXPECTED_GRID: np.ndarray = np.array([0, 0, 0])
+
+# check if bad res for init trigger normal behaviour, in regards to the max res possible
+BAD_RES = [4, 7]
