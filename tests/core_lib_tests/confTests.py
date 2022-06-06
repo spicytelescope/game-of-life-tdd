561cfba7282f108489bac5f3592e050f5ba16fef
spicytelescope <jbeziaud@gmail.com>
2022-06-07 00:17:09 +0200

    feat: adding tox

--- tests/core_lib_tests/confTests.py
+++ tests/core_lib_tests/confTests.py
@@ -2,13 +2,18 @@
 Configuration for core lib tests
 """
 import numpy as np
-from typing import List
 
 # Unit testing
 
 # Checking good behaviour of the core function after n turns
 INIT_GRID: np.ndarray = np.array([[0, 0, 1]])
 EXPECTED_GRID: np.ndarray = np.array([])  # create a mock result that shoudln't
-N_TURN = 5
+N_TURN: int = 5
 
-# Checking expected crash for incorrect values
+# Checking expected crash for incorrect values in the core grid
+INCORRECT_INIT_GRID: np.ndarray = np.array([[3, 2, 1]])
+
+# Checking core grid behaviour with a huge number of turn
+LOAD_TEST_N_TURN: int = 100_000_000
+LOAD_TEST_INIT_GRID: np.ndarray = np.array([[0, 0, 1]])
+LOAD_TEST_EXPECTED_GRID: np.ndarray = np.array([])  # create a mock result that shoudln't
